b7d6c4b1babf0461fdabf8d7ce58b0b7377b44d9
Chris Hart <chrstphrhrt@gmail.com>
2020-06-07 23:08:19 -0400

    adds time package for more robust logic

--- shift.go
+++ shift.go
@@ -3,12 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"strconv"
+	"sort"
+	"time"
 )
 
+const timeLayout = "1504"
+
 type shift struct {
-	Start int64
-	End   int64
+	Start time.Time
+	End   time.Time
 }
 
 type shiftSource struct {
@@ -23,8 +26,8 @@ func (s *shift) UnmarshalJSON(j []byte) error {
 		return err
 	}
 	// fmt.Println(shiftSourceData)
-	s.Start, _ = strconv.ParseInt(shiftSourceData.Start, 10, 64)
-	s.End, _ = strconv.ParseInt(shiftSourceData.End, 10, 64)
+	s.Start, _ = time.Parse(timeLayout, shiftSourceData.Start)
+	s.End, _ = time.Parse(timeLayout, shiftSourceData.End)
 	return nil
 }
 
@@ -34,69 +37,76 @@ func main() {
 	if errUserShifts != nil {
 		fmt.Println("error:", errUserShifts)
 	}
+	sort.Slice(userShifts, func(i, j int) bool {
+		return userShifts[i].Start.Before(userShifts[j].Start)
+	})
+	// for _, shift := range userShifts {
+	// 	fmt.Println(
+	// 		"userShift start", shift.Start.Format(timeLayout),
+	// 		"end", shift.End.Format(timeLayout))
+	// }
 	// fmt.Printf("userShifts, %+v", userShifts)
+
+	midnight, _ := time.Parse(timeLayout, "0000")
+	var unscheduledPeriods []shift
+	for i, uShift := range userShifts {
+		if i == 0 && uShift.Start.After(midnight) {
+			firstPeriod := shift{midnight, uShift.Start}
+			unscheduledPeriods = append(unscheduledPeriods, firstPeriod)
+		} else {
+			nextPeriod := shift{userShifts[i-1].End, uShift.Start}
+			unscheduledPeriods = append(unscheduledPeriods, nextPeriod)
+		}
+		if i == len(userShifts)-1 {
+			lastPeriod := shift{uShift.End, midnight.Add(-time.Second)}
+			unscheduledPeriods = append(unscheduledPeriods, lastPeriod)
+		}
+	}
+	// for _, period := range unscheduledPeriods {
+	// 	fmt.Println(
+	// 		"unscheduledPeriod start", period.Start.Format(timeLayout),
+	// 		"end", period.End.Format(timeLayout))
+	// }
 
 	var availableShifts []shift
 	errAvailShifts := json.Unmarshal(availableShiftData, &availableShifts)
 	if errAvailShifts != nil {
 		fmt.Println("error:", errAvailShifts)
 	}
+	sort.Slice(availableShifts, func(i, j int) bool {
+		return availableShifts[i].Start.Before(availableShifts[j].Start)
+	})
+	// for _, shift := range availableShifts {
+	// 	fmt.Println(
+	// 		"availableShift start", shift.Start.Format(timeLayout),
+	// 		"end", shift.End.Format(timeLayout))
+	// }
 	// fmt.Printf("availableShifts %+v", availableShifts)
 
 	for _, shift := range availableShifts {
-		shiftIsValid := isValidShift(userShifts, shift)
+		shiftIsValid := isValidShift(unscheduledPeriods, shift)
 		// fmt.Println("shiftIsValid", shiftIsValid)
 		if shiftIsValid {
-			fmt.Println("shift is valid", shift)
+			fmt.Println(
+				"VALID shift start", shift.Start.Format(timeLayout),
+				"end", shift.End.Format(timeLayout))
 		} else {
-			fmt.Println("shift is invalid", shift)
+			// fmt.Println(
+			// 	"INVALID shift start", shift.Start.Format(timeLayout),
+			// 	"end", shift.End.Format(timeLayout))
 		}
 	}
 
 }
 
-func isValidShift(u []shift, c shift) bool {
-	var exitingStarts []int64
-	var existingEnds []int64
-	for _, shift := range u {
-		exitingStarts = append(exitingStarts, shift.Start)
-		existingEnds = append(existingEnds, shift.End)
-
-		// todo: probably want to check the date packages for helper functions
-	}
-
-	var startOk bool
-	var endOk bool
-
-	// candidate start AFTER any user ends AND candidate ends BEFORE all user starts
-	for _, exEnd := range existingEnds {
-		if c.Start >= exEnd {
-			startOk = true
-			for _, exStart := range exitingStarts {
-				if c.End <= exStart {
-					endOk = true
-				}
-			}
+func isValidShift(unscheduledPeriods []shift, candidate shift) bool {
+	for _, period := range unscheduledPeriods {
+		if candidate.Start.After(period.Start.Add(-time.Minute)) &&
+			candidate.End.Before(period.End.Add(time.Minute)) {
+			return true
 		}
 	}
-
-	// OR
-	// candidate ends BEFORE any user starts AND candidate starts AFTER all user starts
-	for i, exStart := range exitingStarts {
-		if c.End <= exStart {
-			if i >= 1 && c.End <= exitingStarts[i-1] { // lookbehind 1 deep
-				endOk = true
-			}
-			for _, exEnd := range existingEnds {
-				if c.Start >= exEnd || c.Start == 0 {
-					startOk = true
-				}
-			}
-		}
-	}
-
-	// fmt.Println(startOk, endOk)
-	return startOk && endOk
+	return false
 }
 
 var userShiftData = []byte(`[
